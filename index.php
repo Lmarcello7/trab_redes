ca4f900e30c9ff1796638acacddb83830e874642
Lmarcello7 <101017486+Lmarcello7@users.noreply.github.com>
2023-11-14 20:45:03 -0300

    em andamento

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <title>Adivinhe o Número</title>
 </head>
 <body>
-    
+    <button type="button" class="btn btn-success btn-sm" onclick="teste()"></button>
+    <script src="script/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
